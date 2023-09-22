f6cd5d2488e429604641f09a248be6319c5b6dd7
Khip01 <aakhifathallah24@gmail.com>
2023-09-22 20:56:54 +0700

    Add: Controller for API Request Method

---
+++ restaurant_desktop-app/restaurant_desktop-app_master/restaurant_desktop-app/Controller.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restaurant_desktop_app
+{
+    class Controller
+    {
+        // Get All Menus
+        public async Task<MenuResponse> GetMenusDataAsync()
+        {
+            MenuResponse menuResponse = new MenuResponse();
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:8081/api/Menus");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        menuResponse = JsonConvert.DeserializeObject<MenuResponse>(json);
+                    }
+                }
+                catch (Exception er)
+                {
+                    // If Error
+                    MessageBox.Show("Error: " + er.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return menuResponse;
+        }
+
+        // Get one of the menu
+        public async Task<MenuWrapper> GetMenuDataAsync(int id)
+        {
+            MenuWrapper menuWrap = new MenuWrapper();
+
+            using (HttpClient client = new HttpClient()){
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:8081/api/Menu/"+id);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        menuWrap = JsonConvert.DeserializeObject<MenuWrapper>(json);
+                    }
+                }
+                catch (Exception er)
+                {
+                    // If Error
+                    MessageBox.Show("Error: " + er.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+
+            return menuWrap;
+        }
+
+    }
+}
