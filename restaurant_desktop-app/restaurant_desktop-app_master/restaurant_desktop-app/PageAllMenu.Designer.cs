e41f79a59377e0164d9ae371aceeffd2e6f6f7a7
Khip01 <aakhifathallah24@gmail.com>
2023-09-22 20:54:59 +0700

    Feat: Add GET request per menu

--- restaurant_desktop-app/restaurant_desktop-app_master/restaurant_desktop-app/PageAllMenu.Designer.cs
+++ restaurant_desktop-app/restaurant_desktop-app_master/restaurant_desktop-app/PageAllMenu.Designer.cs
@@ -32,8 +32,18 @@ namespace restaurant_desktop_app
             this.lblTitle = new System.Windows.Forms.Label();
             this.dgvMenu = new System.Windows.Forms.DataGridView();
             this.pnlDgv = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblMenuName = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).BeginInit();
             this.pnlDgv.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -53,23 +63,135 @@ namespace restaurant_desktop_app
             this.dgvMenu.AllowUserToDeleteRows = false;
             this.dgvMenu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMenu.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvMenu.Location = new System.Drawing.Point(0, 0);
+            this.dgvMenu.Location = new System.Drawing.Point(0, 54);
             this.dgvMenu.Name = "dgvMenu";
             this.dgvMenu.ReadOnly = true;
             this.dgvMenu.RowHeadersWidth = 51;
             this.dgvMenu.RowTemplate.Height = 50;
-            this.dgvMenu.Size = new System.Drawing.Size(816, 536);
+            this.dgvMenu.Size = new System.Drawing.Size(809, 270);
             this.dgvMenu.TabIndex = 7;
+            this.dgvMenu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMenu_CellContentClick);
             // 
             // pnlDgv
             // 
+            this.pnlDgv.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlDgv.Controls.Add(this.label1);
+            this.pnlDgv.Controls.Add(this.label2);
             this.pnlDgv.Controls.Add(this.dgvMenu);
             this.pnlDgv.Location = new System.Drawing.Point(19, 80);
             this.pnlDgv.Margin = new System.Windows.Forms.Padding(10);
             this.pnlDgv.Name = "pnlDgv";
-            this.pnlDgv.Size = new System.Drawing.Size(816, 536);
+            this.pnlDgv.Size = new System.Drawing.Size(816, 326);
             this.pnlDgv.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(20, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(20, 10, 0, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 31);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "GET:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(81, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(0, 10, 10, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "localhost:8081/api/Menus";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblDescription);
+            this.panel1.Controls.Add(this.lblPrice);
+            this.panel1.Controls.Add(this.lblMenuName);
+            this.panel1.Controls.Add(this.lblId);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(19, 426);
+            this.panel1.Margin = new System.Windows.Forms.Padding(10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(816, 214);
+            this.panel1.TabIndex = 11;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDescription.ForeColor = System.Drawing.Color.DimGray;
+            this.lblDescription.Location = new System.Drawing.Point(20, 143);
+            this.lblDescription.Margin = new System.Windows.Forms.Padding(0, 5, 10, 5);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(136, 31);
+            this.lblDescription.TabIndex = 16;
+            this.lblDescription.Text = "Description:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPrice.ForeColor = System.Drawing.Color.DimGray;
+            this.lblPrice.Location = new System.Drawing.Point(577, 61);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(0, 5, 10, 5);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(69, 31);
+            this.lblPrice.TabIndex = 15;
+            this.lblPrice.Text = "Price:";
+            // 
+            // lblMenuName
+            // 
+            this.lblMenuName.AutoSize = true;
+            this.lblMenuName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMenuName.ForeColor = System.Drawing.Color.DimGray;
+            this.lblMenuName.Location = new System.Drawing.Point(20, 102);
+            this.lblMenuName.Margin = new System.Windows.Forms.Padding(0, 5, 10, 5);
+            this.lblMenuName.Name = "lblMenuName";
+            this.lblMenuName.Size = new System.Drawing.Size(151, 31);
+            this.lblMenuName.TabIndex = 14;
+            this.lblMenuName.Text = "Menu Name: ";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblId.ForeColor = System.Drawing.Color.DimGray;
+            this.lblId.Location = new System.Drawing.Point(20, 61);
+            this.lblId.Margin = new System.Windows.Forms.Padding(0, 5, 10, 5);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(41, 31);
+            this.lblId.TabIndex = 13;
+            this.lblId.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(20, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(20, 10, 0, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 31);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "GET:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(81, 10);
+            this.label4.Margin = new System.Windows.Forms.Padding(0, 10, 10, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(302, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "localhost:8081/api/Menu/:id";
             // 
             // PageAllMenu
             // 
@@ -77,6 +199,7 @@ namespace restaurant_desktop_app
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(854, 676);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlDgv);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -86,6 +209,9 @@ namespace restaurant_desktop_app
             this.Load += new System.EventHandler(this.PageAllMenu_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).EndInit();
             this.pnlDgv.ResumeLayout(false);
+            this.pnlDgv.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,5 +222,14 @@ namespace restaurant_desktop_app
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.DataGridView dgvMenu;
         private System.Windows.Forms.Panel pnlDgv;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblMenuName;
+        private System.Windows.Forms.Label lblId;
     }
 }
