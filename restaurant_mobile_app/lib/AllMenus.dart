b6aafe77b5af3d34dddb8b68af62a454462c481b
Khip01 <aakhifathallah24@gmail.com>
2023-10-01 18:10:06 +0700

    Add Request Controller Http to API

--- restaurant_mobile_app/lib/AllMenus.dart
+++ restaurant_mobile_app/lib/AllMenus.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_mobile_app/main.dart';
+import 'package:http/http.dart' as http;
 
 class AllMenus extends StatelessWidget {
   @override
---
+++ restaurant_mobile_app/lib/controllers/menu_controller.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/menu.dart';
+
+// GET API Req
+Future<Menu> getMenuRequest(String id) async {
+  final uri = Uri.parse("/api/Menu/" + id);
+  final response = await http.get(uri);
+
+  // Check status Request
+  if (response.statusCode == 200) {
+    return Menu.fromJson(json.decode(response.body));
+  }
+
+  throw Exception("Failed to Load Menu");
+}
+
+// POST API Req
+Future<Menu> postMenuRequest(
+    String menuName, String description, int price) async {
+  // Declare Map for store the data value
+  Map<String, dynamic> request = {
+    "menu_name": menuName,
+    "description": description,
+    "price": price,
+  };
+
+  final uri = Uri.parse("localhost:8081/api/Menu");
+  final response = await http.post(uri, body: request);
+
+  // Check status Request
+  if (response.statusCode == 201) {
+    return Menu.fromJson(json.decode(response.body));
+  }
+
+  throw Exception("Failed to Create the Data");
+}
+
+// UPDATE API Req
+Future<Menu> updateMenuRequest(
+    String id, String menuName, String description, int price) async {
+  // Declare Map for store the data value
+  Map<String, dynamic> request = {
+    "menu_name": menuName,
+    "description": description,
+    "price": price,
+  };
+
+  final uri = Uri.parse("localhost:8081/api/Menu/" + id);
+  final response = await http.put(uri, body: request);
+
+// Check status Request
+  if (response.statusCode == 200) {
+    return Menu.fromJson(json.decode(response.body));
+  }
+
+  throw Exception("Failed to Update the Data!");
+}
+
+// DELETE API Req
+Future<Menu?>? deleteMenuRequest(String id) async {
+  final uri = Uri.parse("localhost:8081/api/Menu/" + id);
+  final response = await http.delete(uri);
+
+  // Check status Request
+  if (response.statusCode == 200) {
+    return null;
+  }
+
+  throw Exception("Error Failed to Delete the Data");
+}
