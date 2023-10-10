f49b6cb52db7bd29d9969723a740ab4174d2518d
Khip01 <aakhifathallah24@gmail.com>
2023-10-10 20:55:24 +0700

    fix(JSON): returns JSON object structure

--- restaurant_mobile_app/lib/models/menu.dart
+++ restaurant_mobile_app/lib/models/menu.dart
@@ -2,7 +2,7 @@ class Menu {
   final int Id;
   final String MenuName;
   final String Description;
-  final int Price;
+  final double Price;
 
   Menu({
     required this.Id,
@@ -11,10 +11,12 @@ class Menu {
     required this.Price,
   });
 
-  factory Menu.fromJson(Map<String, dynamic> json) => Menu(
-        Id: json["id"],
-        MenuName: json["menu_name"],
-        Description: json["description"],
-        Price: json["price"],
-      );
+  factory Menu.fromJson(Map<String, dynamic> json) {
+    return Menu(
+      Id: json['id'],
+      MenuName: json['menu_name'],
+      Description: json['description'],
+      Price: json['price'].toDouble(),
+    );
+  }
 }
