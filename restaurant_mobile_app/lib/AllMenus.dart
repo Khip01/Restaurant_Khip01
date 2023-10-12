d447eb9d45ab6c7c01a3b231bf6e26ecab63f508
Khip01 <aakhifathallah24@gmail.com>
2023-10-12 12:11:35 +0700

    feat(All Menus): Update Loading With Shimmer, Added display when an API request fails

--- restaurant_mobile_app/lib/AllMenus.dart
+++ restaurant_mobile_app/lib/AllMenus.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:restaurant_mobile_app/controllers/menu_controller.dart';
 import 'package:restaurant_mobile_app/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:restaurant_mobile_app/models/menu.dart';
+import 'package:shimmer/shimmer.dart';
 
 class AllMenus extends StatelessWidget {
   @override
@@ -57,23 +59,86 @@ class AllMenus extends StatelessWidget {
                 );
               });
         } else {
-          return Container(
-            child: Stack(children: [
-              Positioned.fill(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircularProgressIndicator(),
-                      Container(
-                          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                          child: Text("Failed to load the data,\ncheck the API connection!", textAlign: TextAlign.center,)),
-                    ],
+          // return Container(
+          //   child: Stack(children: [
+          //     Positioned.fill(
+          //       child: Container(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           crossAxisAlignment: CrossAxisAlignment.center,
+          //           children: [
+          //             CircularProgressIndicator(),
+          //             Container(
+          //                 padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+          //                 child: Text("Failed to load the data,\ncheck the API connection!", textAlign: TextAlign.center,)),
+          //           ],
+          //         ),
+          //       ),
+          //     ),
+          //   ]),
+          // );
+          if (snapshot.hasError) {
+            return Container(
+              child: Stack(children: [
+                Positioned.fill(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                              "404\nNOT FOUND",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+                            )),
+                        SvgPicture.asset(
+                          'assets/NotFound.svg',
+                          semanticsLabel: 'Not Found 404',
+                          width: 300,
+                        ),
+                        Container(
+                            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                            child: Text(
+                              "Failed to load the data, check the API connection!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )),
+                      ],
+                    ),
                   ),
                 ),
+              ]),
+            );
+          }
+          return Container(
+            height: 720,
+            width: double.infinity,
+            child: Shimmer.fromColors(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(10, 5, 5, 10),
+                    height: 180,
+                    child: Card(
+                      elevation: 5,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [],
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            ]),
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+            ),
           );
         }
       },
