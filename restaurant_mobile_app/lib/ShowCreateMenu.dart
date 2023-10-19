c0f29bf479d9ad62faa55ede18c4e092305d8f85
Khip01 <aakhifathallah24@gmail.com>
2023-10-19 11:56:45 +0700

    feat(Create Menu): New Menu Cover Section

---
+++ restaurant_mobile_app/lib/ShowCreateMenu.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class ShowCreateMenu extends StatelessWidget {
+  const ShowCreateMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hello Create Data"),
+    );
+  }
+}
