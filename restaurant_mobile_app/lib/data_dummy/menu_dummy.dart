26f871fa40770596b1d18a8f0e58bd27617b6c70
Khip01 <aakhifathallah24@gmail.com>
2023-12-09 21:05:02 +0700

    feat: Added Data Dummy for Display Mode

---
+++ restaurant_mobile_app/lib/data_dummy/menu_dummy.dart
@@ -0,0 +1,13 @@
+class MenuDummy {
+
+  static String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat ipsum nec enim condimentum tempor. Proin pretium, neque eget dignissim mollis, purus augue faucibus tortor, vitae ornare nibh nisl quis massa. Etiam est odio, ornare id ullamcorper at, efficitur ac ex.";
+
+  List<Map> menu = [
+    {"menu_name": "Bakso", "description": "${lorem}", "price": "9999"},
+    {"menu_name": "Pangsit", "description": "${lorem}", "price": "9999"},
+    {"menu_name": "Mie Ayam", "description": "${lorem}", "price": "9999"},
+    {"menu_name": "Nasi Goreng", "description": "${lorem}", "price": "9999"},
+    {"menu_name": "Ayam Goreng", "description": "${lorem}", "price": "9999"},
+  ];
+
+}
