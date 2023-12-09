4f981798fb9e69a847b3e0f9d158058079072f67
Khip01 <aakhifathallah24@gmail.com>
2023-12-09 20:55:38 +0700

    feat: Add Switch To API Mode, Change Theme Use Material Theme 3, Change Navbar Style and Route

--- restaurant_mobile_app/lib/main.dart
+++ restaurant_mobile_app/lib/main.dart
@@ -3,10 +3,13 @@ import 'package:restaurant_mobile_app/AllMenus.dart';
 import 'package:restaurant_mobile_app/CreateMenu.dart';
 import 'package:restaurant_mobile_app/Deletemenu.dart';
 import 'package:restaurant_mobile_app/UpdateMenu.dart';
+import 'package:restaurant_mobile_app/Utils/util.dart';
 
-void main() {
+void main() async {
   runApp(const MainApp());
 }
+
+bool switchIsApiMode = false; // Switch Mode
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -16,6 +19,16 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  Util util = new Util();
+
+  @override
+  void initState() {
+    super.initState();
+    util.setApiAddress("192.168.88.121");
+    util.setIsActiveIndex(0);
+    initMode();
+  }
+
   // Declare default index for state
   int _selectedIndex = 0;
 
@@ -23,7 +36,7 @@ class _MainAppState extends State<MainApp> {
   List<bool> _isSelected = List.generate(4, (i) => i == 0);
 
   // List that use for declare Title
-  String titleApp = "All Menus";
+  List<String> _titleApp = ["All Menu", "Create Menu", "Update Menu", "Delete Menu"];
 
   // Declare state
   List<Widget> _bodies = [
@@ -35,31 +48,58 @@ class _MainAppState extends State<MainApp> {
 
   void onTaped(int index) {
     setState(() {
-      _selectedIndex = index;
+      // _selectedIndex = index;
+      util.setIsActiveIndex(index);
+      util.getIsActiveIndex().then((result) => _selectedIndex = result);
       _isSelected = List.generate(4, (i) => i == index);
-      switch (index) {
-        case 0:
-          titleApp = "All Menu";
-          break;
-        case 1:
-          titleApp = "Create Menu";
-          break;
-        case 2:
-          titleApp = "Update Menu";
-          break;
-        case 3:
-          titleApp = "Delete Menu";
-          break;
-        default:
-      }
+    });
+  }
+
+  // Init untuk mengubah IsApiMode
+  void initMode(){
+    // Init Is API Mode
+    util.getIsApiMode().then((value) {
+      setState(() {
+        switchIsApiMode = value;
+        // debugPrint(value.toString());
+      });
+    });
+    // Init SelectedIndex
+    util.getIsActiveIndex().then((value) {
+      setState(() {
+        _selectedIndex = value;
+      });
+    });
+    // Init Selected Active Tile
+    util.getIsActiveIndex().then((value) {
+      setState(() {
+        _isSelected = List.generate(4, (i) => i == value);
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    initMode(); // Init switch condition is API Mode and isSelectedIndex Page Mode
+    // Thumb Icon for Switch Check/Close
+    final MaterialStateProperty<Icon?> thumbIcon =
+        MaterialStateProperty.resolveWith<Icon?>(
+      (Set<MaterialState> states) {
+        if (states.contains(MaterialState.selected)) {
+          return const Icon(Icons.check);
+        }
+        return const Icon(Icons.close);
+      },
+    );
+
     return MaterialApp(
+      initialRoute: "/",
       theme: ThemeData(
-        primarySwatch: Colors.brown,
+        useMaterial3: true,
+        // cardTheme: CardTheme(color: Colors.white),
+        // colorScheme: ,
+        colorSchemeSeed: Color.fromARGB(255, 255, 244, 240),
+        // primarySwatch: Colors.brown,
         fontFamily: "Quicksand",
         scaffoldBackgroundColor: Color.fromARGB(255, 245, 245, 245),
       ),
@@ -67,99 +107,149 @@ class _MainAppState extends State<MainApp> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text(
-            titleApp,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            _titleApp[_selectedIndex],
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
+          iconTheme: IconThemeData(color: Colors.white),
           backgroundColor: Colors.brown[600],
         ),
         body: _bodies[_selectedIndex],
         drawer: Drawer(
-          backgroundColor: Color.fromARGB(255, 198, 178, 169),
-          child: ListView(
+          backgroundColor: Colors.brown,
+          child: Container(
             // Remove padding from list view
             padding: EdgeInsets.all(0),
-            children: [
-              const DrawerHeader(
-                margin: EdgeInsets.zero,
-                decoration: BoxDecoration(
-                  color: Colors.brown,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   children: [
-                    Text(
-                      "Khip01 Restaurant",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+                    const DrawerHeader(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Khip01 Restaurant",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)
+                            ),
+                            selected: _isSelected[0],
+                            selectedTileColor: Color.fromARGB(255, 175, 145, 140),
+                            leading: Image(image: AssetImage("assets/Index24.png")),
+                            title: const Text(
+                              "All Menus",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onTap: () {
+                              // Update the state of the app
+                              onTaped(0);
+                              // Then Close The Drawer
+                              // Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            tileColor: _isSelected[1]
+                                ? Color.fromARGB(255, 175, 145, 140)
+                                : null,
+                            leading: Image(image: AssetImage("assets/Create24.png")),
+                            title: const Text(
+                              "Create Menu",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onTap: () {
+                              // Update the state of the app
+                              onTaped(1);
+                              // Then Close The Drawer
+                              // Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)
+                            ),
+                            tileColor: _isSelected[2]
+                                ? Color.fromARGB(255, 175, 145, 140)
+                                : null,
+                            leading: Image(image: AssetImage("assets/Edit24.png")),
+                            title: const Text(
+                              "Update Menu",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onTap: () {
+                              // Update the state of the app
+                              onTaped(2);
+                              // Then Close The Drawer
+                              // Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)
+                            ),
+                            tileColor: _isSelected[3]
+                                ? Color.fromARGB(255, 175, 145, 140)
+                                : null,
+                            leading: Image(image: AssetImage("assets/remove24.png")),
+                            title: const Text(
+                              "Delete Menu",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onTap: () {
+                              // Update the state of the app
+                              onTaped(3);
+                              // Then Close The Drawer
+                              // Navigator.pop(context);
+                            },
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ),
-              ListTile(
-                selected: _isSelected[0],
-                selectedTileColor: Color.fromARGB(255, 175, 145, 140),
-                leading: Image(image: AssetImage("assets/Index24.png")),
-                title: const Text(
-                  "All Menus",
-                  style: TextStyle(color: Colors.white),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: Column(
+                    children: [
+                      Switch(
+                        thumbIcon: thumbIcon,
+                        value: switchIsApiMode,
+                        onChanged: (value) {
+                          setState(() {
+                            switchIsApiMode = value;
+                            util.setIsApiMode(value);
+                          });
+                        },
+                      ),
+                      Text(
+                        "Change to API Mode",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
-                onTap: () {
-                  // Update the state of the app
-                  onTaped(0);
-                  // Then Close The Drawer
-                  // Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                tileColor:
-                    _isSelected[1] ? Color.fromARGB(255, 175, 145, 140) : null,
-                leading: Image(image: AssetImage("assets/Create24.png")),
-                title: const Text(
-                  "Create Menu",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  // Update the state of the app
-                  onTaped(1);
-                  // Then Close The Drawer
-                  // Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                tileColor:
-                    _isSelected[2] ? Color.fromARGB(255, 175, 145, 140) : null,
-                leading: Image(image: AssetImage("assets/Edit24.png")),
-                title: const Text(
-                  "Update Menu",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  // Update the state of the app
-                  onTaped(2);
-                  // Then Close The Drawer
-                  // Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                tileColor:
-                    _isSelected[3] ? Color.fromARGB(255, 175, 145, 140) : null,
-                leading: Image(image: AssetImage("assets/remove24.png")),
-                title: const Text(
-                  "Delete Menu",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  // Update the state of the app
-                  onTaped(3);
-                  // Then Close The Drawer
-                  // Navigator.pop(context);
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
