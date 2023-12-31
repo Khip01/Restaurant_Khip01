c474e8f24893669f90e87f7c68a0de7f2c9b54ab
Khip01 <aakhifathallah24@gmail.com>
2023-12-31 21:23:46 +0700

    chore: prep before fetching data from API

--- restaurant_mobile_app/lib/UpdateMenu.dart
+++ restaurant_mobile_app/lib/UpdateMenu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_mobile_app/ShowUpdateMenu.dart';
+import 'package:restaurant_mobile_app/controllers/menu_controller.dart';
 import 'package:restaurant_mobile_app/data_dummy/menu_dummy.dart';
 import 'package:restaurant_mobile_app/data_dummy/type_menu_dummy.dart';
 
@@ -7,6 +8,8 @@ class UpdateMenu extends StatefulWidget {
   @override
   State<UpdateMenu> createState() => _UpdateMenuState();
 }
+
+bool isApiMode = true;
 
 class _UpdateMenuState extends State<UpdateMenu> {
   MenuDummy menuDummy = new MenuDummy();
@@ -26,167 +29,194 @@ class _UpdateMenuState extends State<UpdateMenu> {
   }
 
   Widget build(BuildContext context) {
+    if (isApiMode){
+      return FutureBuilder(
+        future: getMenusRequest(),
+        builder: (context, snapshot){
+          if(snapshot.hasError)
+            return ErrorUpdatePage();
+          else if (snapshot.hasData)
+            return UpdatePage(snapshot);
+          else
+            return ShimmerUpdatePage();
+        },
+      );
+    } else
+      return UpdatePage();
+  }
+
+  Widget ErrorUpdatePage(){
+    return SizedBox(
+      child: Text("Ini Error"),
+    );
+  }
+
+  Widget ShimmerUpdatePage(){
+    return SizedBox(
+      child: Text("Ini Shimmer"),
+    );
+  }
+
+  Widget UpdatePage([AsyncSnapshot? snapshot]) {
     return Stack(
       children: [
         Container(
           padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
           child: Column(
             children: [
-              Flexible(
-                flex: 1,
-                child: ListView.builder(
-                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: typeMenuDummy.typeMenu.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: typeMenuDummy.typeMenu[index]
-                                  ["isSelected"]
-                              ? Color.fromARGB(255, 175, 145, 140)
-                              : Colors.brown,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            // Set isSelected untuk item yang ditekan
-                            typeMenuDummy.typeMenu[index]["isSelected"] =
-                                !typeMenuDummy.typeMenu[index]["isSelected"];
-                            // Mengurutkan berdasarkan abjad
-                            typeMenuDummy.typeMenu
-                                .sort((a, b) => a["Type"].compareTo(b["Type"]));
-                            // Menyortir kembali daftar setelah mengubah nilai
-                            typeMenuDummy.typeMenu.sort(
-                                (a, b) => (a["isSelected"] == b["isSelected"])
-                                    ? 0
-                                    : a["isSelected"]
-                                        ? -1
-                                        : 1);
-                          });
-                        },
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: Text(
-                                typeMenuDummy.typeMenu[index]["Type"],
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            if (typeMenuDummy.typeMenu[index]["isSelected"])
-                              Icon(
-                                Icons.close,
-                                color: Colors.white,
-                              ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Flexible(
-                flex: 8,
-                child: ListView.builder(
-                  itemCount: menuDummy.menu.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
-                          height: 180,
-                          child: Card(
-                            clipBehavior: Clip.antiAlias,
-                            elevation: 5,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  flex: 3,
-                                  child: Container(
-                                    padding:
-                                        EdgeInsets.fromLTRB(20, 20, 20, 20),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text(
-                                          menuDummy.menu[index]["menu_name"],
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20),
-                                        ),
-                                        Container(
-                                          height: 65,
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                "Desc: ",
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                              Text(
-                                                menuDummy.menu[index]
-                                                    ["description"],
-                                                maxLines: 2,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        Text(
-                                          "Rp. " +
-                                              menuDummy.menu[index]["price"]
-                                                  .toString(),
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: Colors.lightGreen,
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Flexible(
-                                  flex: 1,
-                                  child: Container(
-                                    height: double.maxFinite,
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Color.fromARGB(
-                                              255, 198, 178, 169),
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(0))),
-                                      child: Icon(
-                                        Icons.edit_square,
-                                        color: Colors.brown,
-                                      ),
-                                      onPressed: () =>
-                                          _showModalBottomSheet(context, index),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ),
+              SortSection(),
+              ListViewBodySection(snapshot),
             ],
           ),
         ),
       ],
+    );
+  }
+
+  Widget ListViewBodySection(AsyncSnapshot? snapshot) {
+    return Flexible(
+      flex: 8,
+      child: ListView.builder(
+        itemCount: isApiMode ? snapshot?.data["All Menu"].length : menuDummy.menu.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                height: 180,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        flex: 3,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                menuDummy.menu[index]["menu_name"],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Container(
+                                height: 65,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Desc: ",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      menuDummy.menu[index]["description"],
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                "Rp. " +
+                                    menuDummy.menu[index]["price"].toString(),
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.lightGreen,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          height: double.maxFinite,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 198, 178, 169),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0))),
+                            child: Icon(
+                              Icons.edit_square,
+                              color: Colors.brown,
+                            ),
+                            onPressed: () =>
+                                _showModalBottomSheet(context, index),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget SortSection() {
+    return Flexible(
+      flex: 1,
+      child: ListView.builder(
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+        scrollDirection: Axis.horizontal,
+        itemCount: typeMenuDummy.typeMenu.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: typeMenuDummy.typeMenu[index]["isSelected"]
+                    ? Color.fromARGB(255, 175, 145, 140)
+                    : Colors.brown,
+              ),
+              onPressed: () {
+                setState(() {
+                  // Set isSelected untuk item yang ditekan
+                  typeMenuDummy.typeMenu[index]["isSelected"] =
+                      !typeMenuDummy.typeMenu[index]["isSelected"];
+                  // Mengurutkan berdasarkan abjad
+                  typeMenuDummy.typeMenu
+                      .sort((a, b) => a["Type"].compareTo(b["Type"]));
+                  // Menyortir kembali daftar setelah mengubah nilai
+                  typeMenuDummy.typeMenu
+                      .sort((a, b) => (a["isSelected"] == b["isSelected"])
+                          ? 0
+                          : a["isSelected"]
+                              ? -1
+                              : 1);
+                });
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Text(
+                      typeMenuDummy.typeMenu[index]["Type"],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  if (typeMenuDummy.typeMenu[index]["isSelected"])
+                    Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 
