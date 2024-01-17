638565708e7c69283e8256cd32772e9837de45f6
Khip01 <aakhifathallah24@gmail.com>
2024-01-17 19:36:43 +0700

    chore: remove border, added route to about page

--- restaurant_mobile_app/lib/SettingsScreen.dart
+++ restaurant_mobile_app/lib/SettingsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_mobile_app/settings_screens/about_screen.dart';
 import 'package:restaurant_mobile_app/settings_screens/api_endpoint_screen.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -11,9 +12,9 @@ class SettingsScreen extends StatelessWidget {
         title: Text("Settings"),
         backgroundColor: Colors.transparent,
         elevation: 0,
-        shape: Border(
-          bottom: BorderSide(color: Colors.black, width: 0.2),
-        ),
+        // shape: Border(
+        //   bottom: BorderSide(color: Colors.black, width: 0.2),
+        // ),
       ),
       body: MainScreen(context),
     );
@@ -27,9 +28,9 @@ class SettingsScreen extends StatelessWidget {
             leading: Icon(Icons.edit_location_alt),
             title: Text("Set API Endpoint"),
             trailing: Icon(Icons.arrow_forward_ios_rounded),
-            shape: Border(
-              bottom: BorderSide(color: Colors.grey, width: 0.1),
-            ),
+            // shape: Border(
+            //   bottom: BorderSide(color: Colors.grey, width: 0.1),
+            // ),
             onTap: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => ApiEndpoint()));
             },
@@ -38,11 +39,11 @@ class SettingsScreen extends StatelessWidget {
             leading: Icon(Icons.info),
             title: Text("About this app"),
             trailing: Icon(Icons.arrow_forward_ios_rounded),
-            shape: Border(
-              bottom: BorderSide(color: Colors.grey, width: 0.1)
-            ),
+            // shape: Border(
+            //   bottom: BorderSide(color: Colors.grey, width: 0.1)
+            // ),
             onTap: (){
-              
+              Navigator.push(context, MaterialPageRoute(builder: (context) => About()));
             },
           )
         ],
