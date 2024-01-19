212558742018bae657fab2e44fe2eaea1ac388c6
Khip01 <aakhifathallah24@gmail.com>
2024-01-19 17:06:46 +0700

    feat: change menuDummy to be similar to the API response

--- restaurant_mobile_app/lib/data_dummy/menu_dummy.dart
+++ restaurant_mobile_app/lib/data_dummy/menu_dummy.dart
@@ -2,12 +2,14 @@ class MenuDummy {
 
   static String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat ipsum nec enim condimentum tempor. Proin pretium, neque eget dignissim mollis, purus augue faucibus tortor, vitae ornare nibh nisl quis massa. Etiam est odio, ornare id ullamcorper at, efficitur ac ex.";
 
-  List<Map> menu = [
-    {"menu_name": "Bakso", "description": "${lorem}", "price": "9999"},
-    {"menu_name": "Pangsit", "description": "${lorem}", "price": "9999"},
-    {"menu_name": "Mie Ayam", "description": "${lorem}", "price": "9999"},
-    {"menu_name": "Nasi Goreng", "description": "${lorem}", "price": "9999"},
-    {"menu_name": "Ayam Goreng", "description": "${lorem}", "price": "9999"},
-  ];
+  Map<String, List<Map<String, dynamic>>> menu = {
+    "All Menu": [
+      {"id": "0", "menu_name": "Bakso", "description": "${lorem}", "price": 9999},
+      {"id": "1", "menu_name": "Pangsit", "description": "${lorem}", "price": 9999},
+      {"id": "2", "menu_name": "Mie Ayam", "description": "${lorem}", "price": 9999},
+      {"id": "3", "menu_name": "Nasi Goreng", "description": "${lorem}", "price": 9999},
+      {"id": "4", "menu_name": "Ayam Goreng", "description": "${lorem}", "price": 9999},
+    ]
+  };
 
 }
